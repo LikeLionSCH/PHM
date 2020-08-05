d19e56fefc9f7dc54428142eb996887d18d0397e
thalals <thalsal@naver.com>
2020-08-05 20:42:44 +0900

    DB models

--- PHMapp/models.py
+++ PHMapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Order(models.Model) :
+    image = models.ImageField(upload_to = 'images/')       #이미지
+    name = models.CharField(max_length = 50)
+    price = models.IntegerField();
+    customer = models.CharField(max_length=50)
+    address = models.CharField(max_length=70)
+    request = models.CharField(max_length=50)
+    food = models.CharField(max_length=50)
+    time = models.IntegerField()
+    
+    def __str__(self):
+        return self.name
+
+
