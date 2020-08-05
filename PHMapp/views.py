914cb0a0e886a79b8f2d9f1ccc266c3cac197aa5
thalals <thalsal@naver.com>
2020-08-05 20:43:13 +0900

    views 함수

--- PHMapp/views.py
+++ PHMapp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from .models import Order
 # Create your views here.
+
+def home(request) :
+    orders = Order.objects.all()
+    return render(request, 'home.html',{'orders' : orders})
+
+#주문내역
+def history(request):
+    orders = Order.objects.all()
+    return render(request, 'history.html',{'orders' : orders})
+
+#주문내역 수정
+def edit(request):
+    pass
+
