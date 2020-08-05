ed149e9e5c0d9033bca5e2613ed168b71118b548
thalals <thalsal@naver.com>
2020-08-05 20:43:55 +0900

    urls

--- PJM/urls.py
+++ PJM/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from PHMapp.views import home,edit,history
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('',home, name ="home"),
+    path('edit/',edit, name ="edit"),
+    path('history',history, name ="history"),
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
